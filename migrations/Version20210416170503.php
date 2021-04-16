acc10cf79868e02b04ee4e42c3c9d0ea87b775f3
CSRGH27 <universaltaskmail@gmail.com>
2021-04-16 19:05:21 +0200

    new migrations

---
+++ migrations/Version20210416170503.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210416170503 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, birthdate DATE NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE wage_slip (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, amount DOUBLE PRECISION NOT NULL, company VARCHAR(255) NOT NULL, contributions DOUBLE PRECISION NOT NULL, date_add DATE NOT NULL, date_upd DATE DEFAULT NULL, pdf_file VARCHAR(255) NOT NULL, month INT NOT NULL, year INT NOT NULL, INDEX IDX_1E9C32EBA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE wage_slip ADD CONSTRAINT FK_1E9C32EBA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE wage_slip DROP FOREIGN KEY FK_1E9C32EBA76ED395');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE wage_slip');
+    }
+}
